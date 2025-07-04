b0415254102a6bb5b1173bdbcdb06714639fe584
Rajesh Shahi <rajeshshahi@Rajeshs-MacBook-Air.local>
2025-07-04 12:37:02 +0545

    feat(client): homepage and required function in utils.ts

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,6 +1,50 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from "next";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = "NRP - Your Platform Name",
+  description = "NRP - Your platform description",
+  image = "/assets/preview.png",
+  icons = "/assets/logo.svg",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@yourhandle",
+    },
+    icons,
+    metadataBase: new URL("https://your-production-url.com"),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
